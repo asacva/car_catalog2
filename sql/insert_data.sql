53c1b5bd670141b0327c8cdaaf1a7e45616afb1c
asacva <ahigatori@gmail.com>
2026-02-13 15:17:17 +0300

    feat: add 7 more car records for analysis

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -3,4 +3,12 @@ INSERT INTO cars (brand, model, year, color, price) VALUES
 ('BMW', 'X5', 2022, 'Black', 4500000.50),
 ('Lada', 'Vesta', 2021, 'White', 800000.00);
 
+INSERT INTO cars (brand, model, year, color, price) VALUES
+('Ford', 'Mustang', 2017, 'Red', 5500000.00),
+('Audi', 'Sport', 2025, 'Black', 5000000.50),
+('Toyota', 'Camry', 2016, 'Grey', 3000000.00),
+('Hyndai', 'Solaris', 2018, 'White', 1500000.00),
+('Tesla', 'Cybertruck', 2023, 'Silver', 6000000.50),
+('Renault', 'X150', 2015, 'Yellow', 650000.00),
+('Kea', 'Rio', 2019, 'Green', 480000.00);
 
